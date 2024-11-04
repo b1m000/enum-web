c54c6e66bd1e0319685d1aa1de53477cf324732d
b1m000 <b1m00@proton.me>
2024-11-04 16:20:33 +0100

    Update enum-web.go

--- enum-web.go
+++ enum-web.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 	"flag"
 )
 
 var GREEN string = "\033[0;32m"
 var NC string = "\033[0m"
 
+//name of OS
+var OS string = runtime.GOOS
+
 func main(){
-	logo()
 	flag.Usage = usage
 	flag.Parse()
 
@@ -21,11 +24,15 @@ func main(){
 		fmt.Println("ERROR: missing url (--help)")
 		os.Exit(1)
 	}
+	logo()
 	url := args[0]
 	fmt.Println("Target:", url)
-
+	
 	fmt.Print("Open Firefox to wayback machine.")
 	runF(url,"w")
+	
+	fmt.Print("Open Firefox to the site:", url)
+	runF(url,"s")	
 
 	fmt.Print("Open Firefox using google dorking.")
 	runF(url,"g")
@@ -50,7 +57,7 @@ func fuff(url string){
 		fmt.Printf("Running ffuf with wordlist: %s\n", wordlist)
 
 		// Construct and execute the ffuf command
-		cmd := exec.Command("ffuf", "-u", url, "-w", wordlist,"-s")
+		cmd := exec.Command("ffuf", "-u", url, "-w", wordlist, "-fc", "302")
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			fmt.Printf("Error running ffuf with wordlist %s: %v\n", wordlist, err)
@@ -63,25 +70,69 @@ func fuff(url string){
 }
 
 func runF(url string, tipe string){
-	switch(tipe){
-		case "g":
-			urlw := "https://www.google.com/search?client=firefox-b-d&q=site%3A" + url[8:]
-			cmd := exec.Command("/Applications/Firefox.app/Contents/MacOS/firefox",urlw)
-			err := cmd.Start()
-			if err != nil {
-				fmt.Println("Error:", err)
+	switch(OS){
+		//LINUX
+		case "linux":
+			switch(tipe){
+				case "g":
+					urlw := "https://www.google.com/search?client=firefox-b-d&q=site%3A" + url[8:] + "+|+|+\"" + url[8:] + "\""
+					cmd := exec.Command("firefox",urlw)
+					err := cmd.Start()
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+					fmt.Println(" Done ✅\a")
+				case "w":
+					urlw := "https://web.archive.org/web/*/"+url+"*"
+					cmd := exec.Command("firefox",urlw)
+					err := cmd.Start()
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+					fmt.Println(" Done ✅\a")
+				case "s":
+					cmd := exec.Command("firefox",url)
+								err := cmd.Start()
+								if err != nil {
+										fmt.Println("Error:", err)
+								}
+								fmt.Println(" Done ✅\a")
+				default:
+					fmt.Println("Error")
 			}
-			fmt.Println(" Done ✅\a")
-		case "w":
-			urlw := "https://web.archive.org/web/*/"+url+"*"
-			cmd := exec.Command("/Applications/Firefox.app/Contents/MacOS/firefox",urlw)
-			err := cmd.Start()
-			if err != nil {
-				fmt.Println("Error:", err)
+
+		//MAC 
+		case "darwin":
+			switch(tipe){
+				case "g":
+					urlw := "https://www.google.com/search?client=firefox-b-d&q=site%3A" + url[8:] + "+|+|+\"" + url[8:] + "\""
+					cmd := exec.Command("/Applications/Firefox.app/Contents/MacOS/firefox",urlw)
+					err := cmd.Start()
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+					fmt.Println(" Done ✅\a")
+				case "w":
+					urlw := "https://web.archive.org/web/*/"+url+"*"
+					cmd := exec.Command("/Applications/Firefox.app/Contents/MacOS/firefox",urlw)
+					err := cmd.Start()
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+					fmt.Println(" Done ✅\a")
+				case "s":
+					cmd := exec.Command("/Applications/Firefox.app/Contents/MacOS/firefox",url)
+								err := cmd.Start()
+								if err != nil {
+										fmt.Println("Error:", err)
+								}
+								fmt.Println(" Done ✅\a")
+				default:
+					fmt.Println("Error")
 			}
-			fmt.Println(" Done ✅\a")
 		default:
-			fmt.Println("Error")
+			fmt.Println("MAC or LINUX")
+			os.Exit(0)
 	}
 }
 
